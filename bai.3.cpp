2aee0ddb5bf63a82ef90bd07f5db972e8a6e8fa0
ADMIN <ADMIN@LAB1-MAY03>
2024-02-26 08:30:48 +0700

    Add project files.

---
+++ bai.3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int so1, so2, tong;
+	cout << "CHUONG TRiNH TINH TONG\n";
+	cout << "Nhap so thu nhat:";
+	cin >> so1;
+	cout << "nhap so thu hai:";
+	cin >> so2;
+	tong = so1 + so2;
+	cout << so1 << "+" << so2 << "="
+		<< tong
+		<< endl;
+	return 0;
+}
+
